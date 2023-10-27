855b46c328a9bca9e8d491c5d9783370050838bb
cath-funtown <cath@funtownstudio.com>
2023-10-27 09:22:24 +0200

    neuroemote changes

--- Desktop/dev/experience-foundry-new/.next/static/chunks/webpack.js
+++ Desktop/dev/experience-foundry-new/.next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "834ae3794b2467a6"; }
+/******/ 		__webpack_require__.h = function() { return "0311971ed5533b22"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
